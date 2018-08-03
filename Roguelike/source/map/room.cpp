a032cfff859e004bb3ce1699641ea02c039e2839
ElroyDolleman <elroydolleman@gmail.com>
2018-08-03 22:51:54 +0200

    Added functions to conver world to grid location and vice versa

--- Roguelike/source/map/room.cpp
+++ Roguelike/source/map/room.cpp
@@ -1,6 +1,9 @@
 #include "stdafx.h"
-#include "constants.h"
 #include "map\room.h"
+
+#include "constants.h"
+#include "map\tile.h"
+#include "entities\entity.h"
 
 Room::Room(Tileset tileset, int roomWidth, int roomHeight)
 	: tileset(tileset)
@@ -17,7 +20,7 @@ Room::~Room()
 {
 }
 
-void Room::CreateBackgroundTiles(const vector<int>& tileNumbers, const json& tilesetData)
+void Room::CreateTiles(const vector<int>& tileNumbers, const json& tilesetData)
 {
 	for (int i = 0; i < tileNumbers.size(); i++)
 	{
@@ -31,6 +34,34 @@ void Room::CreateBackgroundTiles(const vector<int>& tileNumbers, const json& til
 		tileSprite.setTileNumber(tileNumbers[i] - 1);
 		tiles[y].push_back(new Tile({ x*grid::size, y*grid::size, grid::size, grid::size }, tileSprite, tilesetData));
 	}
+}
+
+Tile* Room::GetTile(int x, int y) const
+{
+	if (x < 0 || x >= size.x || y < 0 || y >= size.y)
+		return nullptr;
+
+	return tiles[y][x];
+}
+
+Vector2i Room::ToGridLocation(float x, float y) const
+{
+	return Vector2i((int)floor(x / grid::size), (int)floor(y / grid::size));
+}
+
+Vector2i Room::ToGridLocation(Vector2f worldPosition) const
+{
+	return ToGridLocation(worldPosition.x, worldPosition.y);
+}
+
+Vector2f Room::ToWorldPosition(int x, int y) const
+{
+	return Vector2f();
+}
+
+Vector2f Room::ToWorldPosition(const Vector2i& gridLocation) const
+{
+	return Vector2f();
 }
 
 void Room::Draw(RenderWindow& window)
